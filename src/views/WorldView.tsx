a38f98251534c9e4371edd5272ac25cf81d6fb3d
Tu Dat Nguyen <tudat@affinitybridge.com>
2020-04-05 08:35:00 -0700

    Render basic map on WorldView

--- src/views/WorldView.tsx
+++ src/views/WorldView.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
+import React, {useRef, useReducer} from 'react';
+import {StyleSheet, View} from 'react-native';
 import {Container, Text} from 'native-base';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+import {MAPBOX_TOKEN} from '../../env';
+
+MapboxGL.setAccessToken(MAPBOX_TOKEN);
 
 export default function WorldView(props: any) {
+  const cameraRef = useRef(null);
+  const mapRef = useRef(null);
   return (
-    <Container>
-      <Text>Some View</Text>
-    </Container>
+    <View style={styles.container}>
+      <MapboxGL.MapView
+        ref={mapRef}
+        styleURL="mapbox://styles/tudatn/ck823d7tm2gj41is5i4kws2a2"
+        style={styles.map}>
+        <MapboxGL.Camera
+          ref={cameraRef}
+          defaultSettings={{
+            zoomLevel: 2,
+          }}
+        />
+      </MapboxGL.MapView>
+    </View>
   );
 }
 
-const styleSheets = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  map: {
     flex: 1,
   },
 });
